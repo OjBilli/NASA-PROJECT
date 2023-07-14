3510f9b73b4940b9cb02194f9abf8ac6a9778d10
Ojbilli <onem08989@gmail.com>
2023-07-14 15:36:10 +0100

    new

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -40,6 +40,17 @@ async function httpSubmitLaunch(launch) {
 
 async function httpAbortLaunch(id) {
   // TODO: Once API is ready.
+  try {
+    return await fetch(`${API_URL}/launches/${id}`, {
+      method: "delete",
+    });
+  
+  } catch (err){
+    return {
+      ok: false,
+    };
+  }
+  
   // Delete launch with given ID.
 }
 
