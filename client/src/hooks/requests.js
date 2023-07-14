a7a3d745aa0bbf64ac7065cb988d60a3977eef00
Ojbilli <onem08989@gmail.com>
2023-07-14 14:43:14 +0100

    new

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -19,6 +19,22 @@ async function httpGetLaunches() {
 
 async function httpSubmitLaunch(launch) {
   // TODO: Once API is ready.
+  try {
+    return await fetch(`${API_URL}/launches`, {
+      method: "post",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(launch),
+    })
+  
+  } catch (err){
+    return {
+      ok: false,
+    };
+  }
+
+  
   // Submit given launch data to launch system.
 }
 
