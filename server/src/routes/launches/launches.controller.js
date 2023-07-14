736f94544627849536e6a71c9cae1d2adf2034e6
Ojbilli <onem08989@gmail.com>
2023-07-14 01:15:21 +0100

    new

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,9 +1,24 @@
-const {getAllLaunches} = require('../../models/launches.model');
+const {
+    getAllLaunches,
+    addNewLaunch,
+} = require('../../models/launches.model');
 
-function HttpGetAllLaunches (req, res) {
+function httpGetAllLaunches (req, res) {
     return res.status(200).json(getAllLaunches());
 }
 
+function httpAddNewLaunch (req, res) {
+
+    const launch = req.body;
+
+    launch.launchDate = new Date(launch.launchDate);
+    
+
+    addNewLaunch(launch);
+    return res.status(201).json(launch);
+}
+
 module.exports = {
-    HttpGetAllLaunches,
+    httpGetAllLaunches,
+    httpAddNewLaunch,
 };
--- server/src/routes/launches/launches.router.js
+++ server/src/routes/launches/launches.router.js
@@ -1,10 +1,12 @@
 const express = require ('express');
 const {
-    HttpGetAllLaunches,
+    httpGetAllLaunches,
+    httpAddNewLaunch,
 } = require ('./launches.controller');
 
 const launchesRouter = express.Router();
 
-launchesRouter.get('/launches', HttpGetAllLaunches);
+launchesRouter.get('/', httpGetAllLaunches);
+launchesRouter.post('/', httpAddNewLaunch);
 
 module.exports = launchesRouter;
