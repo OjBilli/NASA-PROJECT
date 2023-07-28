3063932bb3e6f248709c1228cfc1034600a3f81f
Ojbilli <onem08989@gmail.com>
2023-07-28 10:37:26 +0100

    new

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,15 @@
+describe('Test GET /launches', () => {
+    test('It should respond with 200 success', () => {
+        const response = 200;
+        expect(response).toBe(200);
+    });
+});
+
+describe('Test Post /launches', () => {
+    test('It should respond with 200 success', () => {
+
+    });
+
+    test('It should catch missing properties', () => {});
+    test('It should catch invalid dates', () => {});
+})
